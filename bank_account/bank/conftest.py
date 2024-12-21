0769ab34d0a27bb5c958315e77dde0e62a1506f0
Degisew <jegisew21@gmail.com>
2024-12-21 14:35:36 +0300

    Fix: remove unwanted docs

--- bank_account/bank/conftest.py
+++ bank_account/bank/conftest.py
@@ -9,17 +9,14 @@ User = get_user_model()
 
 @pytest.fixture
 def api_client() -> APIClient:
-    """Fixture to provide an API client."""
     return APIClient()
 
 
 @pytest.fixture
 def user():
-    """Fixture to create a user."""
     return baker.make(User)
 
 
 @pytest.fixture
 def bank_account(user) -> BankAccount:
-    """Fixture to create a bank account for the user."""
     return baker.make(BankAccount, user=user, balance=500.00)
